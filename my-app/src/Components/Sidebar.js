383b96a203ebcb64ddc86bad8a2463dbc5bf880d
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-12-17 14:17:14 +0530

    sidebar

--- my-app/src/Components/Sidebar.js
+++ my-app/src/Components/Sidebar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Sidebar.css"
-import coder from "../assets/coder.svg"
-// import FaceIcon from '@mui/icons-material/Face';
+import coder from "../assets/icons/coder.jpg"
+import hackerrank from '../assets/icons/HackerRank.png'
 import linkedin from '../assets/icons/linkedin.png'
 import leetcode from '../assets/icons/leetcode.png'
 import github from '../assets/icons/github.png'
@@ -9,6 +9,8 @@ import location from '../assets/icons/location.png'
 import tie from '../assets/icons/tie.png'
 import email from '../assets/icons/email.png'
 import phone from '../assets/icons/phone.png'
+import portfolio from "../assets/icons/portfolio.png"
+
 
 function Sidebar() {
     const handleEmailMe = () => {
@@ -25,29 +27,45 @@ function Sidebar() {
             <div className='sidebar-title'>
                 SOFTWARE DEVELOPER
             </div>
-            <a href="">
+            <a href="https://drive.google.com/file/d/17sDgWo2pDfWE8kkalPMKTY7HYqcQi-Fp/view">
                 <div className='sidebar-item'>
                     <img src={tie} alt="resume" className='tie-icon' /> Download Resume
                 </div>
             </a>
-            {/* <figure className="social-icons">
-                <a href=""><img src={linkedin} alt="linkedin" className="sidebar__icon" /></a>
-                <a href=""><img src={leetcode} alt="leetcode" className="sidebar__icon" /></a>
-            </figure>
-            <div className="sidebar__contact">
-                <div className="sidebar__item sidebar__github">
-                    <a href=""><img src={github} alt="github" className="sidebar__icon" />github</a>
+            <div className="social-handles">
+                <div className="sidebar__item">
+                    <a href="https://www.linkedin.com/in/japneet-kaur-b-400b60194/"><img src={linkedin} alt="linkedin" className="sidebar__icon" /></a>
+                    Linkedin
                 </div>
-                <div className="sidebar__location">
+                <div className="sidebar__item">
+                    <a href="https://japneetkaur-ff73e.web.app/"><img src={portfolio} alt="portfolio" className="sidebar__icon" /></a>
+                    Portfolio
+                </div>
+                <div className="sidebar__item">
+                    <a href="https://github.com/japneetbhatia"><img src={github} alt="github" className="sidebar__icon" /></a>
+                    Github
+                </div>                
+                <div className="sidebar__item ">
+                    <a href="https://leetcode.com/japneetkaur/"><img src={leetcode} alt="leetcode" className="sidebar__icon" /></a>
+                    Leetcode
+                </div>
+                <div className="sidebar__item">
+                    <a href="https://www.hackerrank.com/japneetk1111"><img src={hackerrank} alt="hackerrank" className="sidebar__icon" /></a>
+                    Hackerrank
+                </div>
+            </div>
+            <div className="sidebar-contact">
+                <div >
                     <img src={location} alt="location" className="sidebar__icon" />
                     Delhi, India
                 </div>
-                <div className="sidebar__item"> <img src={email} alt="email" className="sidebar__icon" />
-                     japneetk1111@gmail.com</div>
-                <div className="sidebar__item"><img src={phone} alt="phone" className="sidebar__icon" />
+                <div><img src={phone} alt="phone" className="sidebar__icon" />
                     8076267548</div>
+                <div> <img src={email} alt="email" className="sidebar__icon" />
+                     japneetk1111@gmail.com</div>
+                
             </div>
-            <div className="sidebar__item sidebar__email" onClick={handleEmailMe}>email me</div> */}
+            <button className="sidebar__email" onClick={handleEmailMe}>CONTACT ME</button>
         </div>
     )
 }
